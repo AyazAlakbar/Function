30d66adf51a7fc69bbefad1e97e9b147c7f34d6e
alexberg90 <ayazya@code.edu.az>
2023-02-15 03:42:41 +0400

    new commit

---
+++ assets/tasks/js/script.js
@@ -0,0 +1,16 @@
+//alert("salam")
+
+
+
+
+function Num(n)
+ {
+    
+    if (n % 21 == 0) {
+      return "eded 21-ə bölünür";
+    } else {
+      return "eded 21-ə bölünmür";
+    }
+    
+  }
+  console.log(Num(42));
