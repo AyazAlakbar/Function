f30b40d51455228f5ba70afcf271f76fba57da50
alexberg90 <ayazya@code.edu.az>
2023-02-15 03:56:59 +0400

    new commit

--- assets/tasks/js/script.js
+++ assets/tasks/js/script.js
@@ -2,15 +2,26 @@
 
 
 
-
+//task 1
 function Num(n)
  {
     
-    if (n % 21 == 0) {
+    if (n % 21 === 0) {
       return "eded 21-ə bölünür";
     } else {
       return "eded 21-ə bölünmür";
     }
     
   }
-  console.log(Num(42));
+  console.log(Num(45));
+
+
+  //task4
+  function Login(email, password) {
+    if (email==="lorem@code.edu.az"&& password==="12345") {
+        return "success"
+    } else {
+        return "email ve ya parol sefdir"
+    }
+}
+  console.log(Login("lorem@code.edu.az", "12345"));
